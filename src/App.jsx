044759428640d894d18d65cf4fda6d0910563367
chenggongcai <1197063758@qq.com>
2019-06-21 18:33:39 +0800

    login登陆界面结构

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Login from '../pages/login';
-import Login from '../pages/admin';
+import Login from './pages/login';
+import Admin from './pages/admin';
 export default class App extends Component {
   render() {
-    return <div>
+    return <Switch>
       <Route path='/login' component={Login}/>
       <Route path='/' component={Admin}/>
-    </div>;
+    </Switch>;
   }
 }
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
-
+import { Form, Icon, Input, Button } from 'antd';
+import logo from './logo.png';
+const Item = Form.Item;
 export default class Login extends Component {
   render() {
     return <div>
-      Login
+      <header className='login-header'>
+        <img src={logo} alt="logo"/>
+        <h1>React项目: 后台管理系统</h1>
+        <section className='login-content'>
+          <h2>用户登录</h2>
+          <Form>
+            <Item>
+              <Input className="login-input" placeholder="用户名" type="text" prefix={<Icon type="user" />}/>
+            </Item>
+            <Item>
+              <Input className="login-input" placeholder="密码" type="password" prefix={<Icon type="lock" />}/>
+            </Item>
+            <Item>
+              <Button type="primary" htmlType="submit" className="login-form-button">登录</Button>
+            </Item>
+          </Form>
+        </section>
+      </header>
     </div>;
   }
 }
