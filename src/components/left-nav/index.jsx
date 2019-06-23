6b3cc0ffb26311161512f7c1ce0386923087ebd2
chenggongcai <1197063758@qq.com>
2019-06-23 15:36:02 +0800

    完成根据menuList数组生成侧边列表

--- src/components/left-nav/index.jsx
+++ src/components/left-nav/index.jsx
@@ -1,17 +1,59 @@
 import React, {Component} from 'react';
 
 import {Icon, Menu} from "antd";
-import {Link} from 'react-router-dom';
+import {Link, withRouter } from 'react-router-dom';
+// withRouter是一个高阶组件用来给非路由组件传递路由组件的三大属性(history,location,match)
 import logo from '../../assets/images/logo.png';
 import './index.less'
 import PropTypes from 'prop-types';
+import menuList from '../../config/menu-config';
 const {SubMenu} = Menu;
-export default class LeftNav extends Component {
+const {Item} = Menu;
+class LeftNav extends Component {
   static propTypes = {
     collapsed: PropTypes.bool.isRequired
+  };
+  addmenu = (menu) => {
+    return <Item key={menu.key}>
+      <Link to={menu.key}>
+        <Icon type={menu.icon}/>
+        <span>{menu.title}</span>
+      </Link>
+    </Item>
   }
+  // componentWillMount一般用来做渲染之前的准备工作,在render之前只做一次
+  // 所以根据登录用户的不同生成不同的侧边列表在componentWillMount完成
+  componentWillMount() {
+
+    this.menus = menuList.map((menu) => {
+      // 缓存
+      const { children } = menu;
+      // 判断菜单等级是一级菜单还是二级菜单
+      if(children) {
+        // 二级菜单
+        return <SubMenu
+          key={menu.key}
+          title={
+            <span>
+                  <Icon type={menu.icon}/>
+                  <span>{menu.title}</span>
+            </span>
+          }
+        >
+          {
+            children.map((item) => this.addmenu(item))
+          }
+        </SubMenu>
+      } else {
+        // 一级菜单
+        return this.addmenu(menu)
+      }
+    });
+    this.selectedKeys = this.props.location.pathname
+  }
+
   render() {
-    const {Item} = Menu;
+
     const {collapsed} = this.props
     return <div>
 
@@ -19,60 +61,12 @@ export default class LeftNav extends Component {
         <img src={logo} alt=""/>
         <h1 style={{display: collapsed ? 'none' : 'block'}}>硅谷后台</h1>
       </Link>
-      <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-        <Item key="1">
-          <Icon type="home"/>
-          <span>首页</span>
-        </Item>
-        <SubMenu
-          key="sub1"
-          title={
-            <span>
-                  <Icon type="appstore"/>
-                  <span>商品</span>
-            </span>
-          }
-        >
-          <Item key="2">
-            <Icon type="bars"/>
-            <span>品类管理</span>
-          </Item>
-          <Item key="3">
-            <Icon type="tool"/>
-            <span>商品管理</span>
-          </Item>
-        </SubMenu>
-        <Item key="4">
-          <Icon type="user"/>
-          <span>用户管理</span>
-        </Item>
-        <Item key="5">
-          <Icon type="safety"/>
-          <span>权限管理</span>
-        </Item>
-        <SubMenu
-          key="sub2"
-          title={
-            <span>
-                  <Icon type="area-chart"/>
-                  <span>图形图表</span>
-                </span>
-          }
-        >
-          <Item key="6">
-            <Icon type="bar-chart"/>
-            <span>柱状图</span>
-          </Item>
-          <Item key="7">
-            <Icon type="line-chart"/>
-            <span>折线图</span>
-          </Item>
-          <Item key="8">
-            <Icon type="pie-chart"/>
-            <span>饼图</span>
-          </Item>
-        </SubMenu>
+      <Menu theme="dark" defaultSelectedKeys={[this.selectedKeys]} mode="inline">
+        {
+          this.menus
+        }
       </Menu>
     </div>;
   }
 }
+export default withRouter(LeftNav)
