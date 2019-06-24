3c13207d6026aa07c90931a8a567be9cfb91fcf5
chenggongcai <1197063758@qq.com>
2019-06-24 18:21:10 +0800

    完成天气和点击哪个菜单项就显示哪个菜单项

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,34 @@
 import ajax from './ajax';
-
+import jsonp from 'jsonp';
+import { message } from 'antd';
 // export const reqLogin = (data) => ajax('/login', data, 'POST');
 // 请求参数3-4个以上使用
 // export const reqLogin = ({username, password}) => ajax('/login', {username, password}, 'POST');
 // 请求参数1-2个使用
 /**
  * 请求登录函数
- * @param username 用户名
- * @param password 密码
- * @returns {返回值一定成功状态promise（请求成功里面有数据，请求失败里面没有）}
+ * username 用户名
+ * password 密码
+ * {返回值一定成功状态promise（请求成功里面有数据，请求失败里面没有）}
  */
+ // 请求登录函数
 export const reqLogin = (username, password) => ajax('/login', {username, password}, 'POST');
+ // 请求验证用户信息
 export const reqValidateUserInfo = (id) => ajax('/validate/user',{id}, 'POST');
+// 请求天气
+export const reqWeather = function() {
+  return new Promise((resolve, reject) => {
+    jsonp(`http://api.map.baidu.com/telematics/v3/weather?location=深圳&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`, {} ,function (err, data) {
+      if (!err) {
+        const { weather, dayPictureUrl } = data.results[0].weather_data[0];
+        resolve({
+          dayPictureUrl,
+          weather
+        })
+      } else {
+          message.error('请求天气失败，请重新刷新试试~');
+          resolve()
+      }
+    })
+  })
+}
--- src/components/haerd/index.jsx
+++ src/components/haerd/index.jsx
@@ -1,27 +1,60 @@
 import React, { Component } from 'react';
 import dayjs from 'dayjs';
-import logo from '../../assets/images/logo.png'
+import logo from '../../assets/images/logo.png';
 import MyButton from '../my-button';
-import { withRouter } from 'react-router-dom'
+import { withRouter } from 'react-router-dom';
+import { reqWeather } from '../../api';
 import './index.less';
 import { removeItem, getItem } from '../../utils/login-tools';
-
+import menuList from '../../config/menu-config';
 import { Modal } from 'antd';
 
 const { confirm } = Modal;
 class HeardMain extends Component {
   state = {
-    sysTime: Date.now()
+    sysTime: Date.now(),
+    weather: '多云',
+    dayPictureUrl: 'http://api.map.baidu.com/images/weather/day/duoyun.png'
   }
   componentWillMount() {
     this.username = getItem().username;
+  };
+  componentWillReceiveProps(nextProps) {
+    this.title = this.getTitle(nextProps)
   }
-  componentDidMount() {
+
+  // 获取当前所在的菜单项
+  getTitle = (nextProps) => {
+    console.log('getTile');
+    const { pathname } = nextProps.location;
+    for (let i = 0; i < menuList.length; i++) {
+      const menu = menuList[i];
+      if (menu.children) {
+        for (let j = 0; j < menu.children.length; j++) {
+          const item = menu.children[j]
+          if (item.key === pathname) {
+            return item.title;
+          }
+        }
+      } else {
+        if (menu.key === pathname) {
+          return menu.title;
+        }
+      }
+    }
+
+  }
+  async componentDidMount() {
     setInterval(() => {
       this.setState({
         sysTime: Date.now()
       })
-    },1000)
+    },1000);
+    // 发送请求天气
+    const result = await reqWeather();
+    if (result) {
+      this.setState(result)
+    }
   }
 
   showConfirm() {
@@ -42,17 +75,18 @@ class HeardMain extends Component {
   }
 
   render() {
+    const { sysTime, dayPictureUrl, weather} = this.state;
     return <div>
       <div className="header-main-top">
         <span>欢迎, {this.username}</span>
         <MyButton onClick={this.showConfirm}>退出</MyButton>
       </div>
       <div className="header-main-bottom">
-        <span className="header-main-left">首页</span>
+        <span className="header-main-left">{this.title}</span>
         <div className="header-main-right">
-          <span>{dayjs(this.state.sysTime).format('YYYY MM-DD-HH:mm:ss')}</span>
-          <img src={logo} alt=""/>
-          <span>多云</span>
+          <span>{dayjs(sysTime).format('YYYY MM-DD-HH:mm:ss')}</span>
+          <img src={dayPictureUrl} alt=""/>
+          <span>{weather}</span>
         </div>
       </div>
     </div>;
